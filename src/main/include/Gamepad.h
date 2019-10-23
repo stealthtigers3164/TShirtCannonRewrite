81a906827ac54aedab903bc6056a44cdec0754bf
Alex <alex.wasylik@gmail.com>
2019-10-23 17:45:57 -0400

    finished buttons, added joystick. Dpad in future update.

--- src/main/include/Gamepad.h
+++ src/main/include/Gamepad.h
@@ -16,54 +16,100 @@ class Gamepad : public Joystick, public JoystickButton, public  JoystickBase{
     
     public:
 
-        enum struct Button{
-            A=1, X=3, LB=5, LT=7, BACK=9,   LSTICK=11,
-            B=2, Y=4, RB=6, RT=8, START=10, RSTICK=12
-        };
-
-        JoystickButton* b_A;
-
-        Joystick *_stick
-
         Gamepad(int port){
-
-            //initialize joystick
-            _stick = new Joystick(port);
-
-            //initialize joystick buttons
-            b_A = new JoystickButton(_stick, 1);
-            b_B = new JoystickButton(_stick, 2);
-            b_X = new JoystickButton(_stick, 3);
-            b_Y = new JoystickButton(_stick, 4);
-            b_LB = new JoystickButton(_stick, 5);
-            b_RB = new JoystickButton(_stick, 6);
-            b_LT = new JoystickButton(_stick, 7);
-            b_RT = new JoystickButton(_stick, 8);
-            b_BACK = new JoystickButton(_stick, 9);
-            b_START = new JoystickButton(_stick, 10);
-            b_LSTICK = new JoystickButton(_stick, 11);
-            b_RSTICK = new JoystickButton(_stick, 12);
+            _gamepad->Joystick(std::static_cast<uint32_t>(port));
         }
 
-        //return true if the specified button is down
-        bool getButtonDown(Button button){
-            return button.Get()
+        //button numbers subject to change
+        //testing to be done in the future
+        bool ButtonA(){
+            return _gamepad.GetRawButton(1);
         }
+        bool ButtonB(){
+            return _gamepad.GetRawButton(2);
+        }
+        bool ButtonX(){
+            return _gamepad.GetRawButton(3);
+        }
+        bool ButtonY(){
+            return _gamepad.GetRawButton(4);
+        }
+        bool LeftBumper(){
+            return _gamepad.GetRawButton(5);
+        }
+        bool RightBumper(){
+            return _gamepad.GetRawButton(6);
+        }
+        bool ButtonLeftJoy(){
+            return _gamepad.GetRawButton(7);
+        }
+        bool ButtonRightJoy(){
+            return _gamepad.GetRawButton(8);
+        }
+        bool ButtonStart(){
+            return _gamepad.GetRawButton(9);
+        }
+        bool ButtonBack(){
+            return _gamepad.GetRawButton(10);
+        }
+
+        const float* LeftJoystick(){
+            float xAxis = _gamepad.GetX(0);
+            float yAxis = _gamepad.GetY(0);
+            return &{xAxis, yAxis};
+        }
+
+        const int* RightJoystick(){
+            float xAxis = _gamepad.GetX(1);
+            float yAxis = _gamepad.GetY(1);
+            return &{xAxis, yAxis};
+        }
+
+        //dpad logic
+        direction DPad(){
+            
+        }
+        
+        //special logic for dpad
+        //return vector for joystick (std::vector)
+
     
     private:
         
-        //joystickbutton variables
-        JoystickButton* b_A;
-        JoystickButton* b_B;
-        JoystickButton* b_X;
-        JoystickButton* b_Y;
-        JoystickButton* b_LB;
-        JoystickButton* b_RB;
-        JoystickButton* b_LT;
-        JoystickButton* b_RT;
-        JoystickButton* b_BACK;
-        JoystickButton* b_START;
-        JoystickButton* b_LSTICK;
-        JoystickButton* b_RSTICK;
+        Joystick* _gamepad;
+
+        enum direction{
+            UP,
+            DOWN,
+            LEFT,
+            RIGHT,
+            UP_LEFT,
+            UP_RIGHT,
+            DOWN_LEFT,
+            DOWN_RIGHT
+        };
+            
+        /* //buttons
+        //main buttons
+        bool B_A;
+        bool B_B;
+        bool B_X;
+        bool B_Y;
+        //bumpers
+        bool B_LB;
+        bool B_RB;
+        //joystick buttons
+        bool B_LSTICK;
+        bool B_RSTICK;
+        //extra buttons
+        bool B_START;
+        bool B_BACK;
+
+        //directional pad
+        dpad_State DPAD;
+
+        //joysticks
+        std::vector J_RIGHT;
+        std::vector J_LEFT; */
 
 }
