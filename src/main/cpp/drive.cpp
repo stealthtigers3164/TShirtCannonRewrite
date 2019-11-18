ef086835e334bc71c8b5a831dd8635b17b8bcd38
Tyson-Chicken-Nuggets <alex.wasylik@gmail.com>
2019-11-18 18:00:53 -0500

    added canges to drive - still wip, kinda bad tbh

--- src/main/cpp/drive.cpp
+++ src/main/cpp/drive.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc/spark.h>
-#include "../include/gamepad.h"
+#include <Gamepad.h>
 
 class drive{
+
+    //create private motor variables
+    private:
+    frc::Spark *m_frontLeft;
+    frc::Spark *m_backLeft;
+    frc::Spark *m_frontRight;
+    frc::Spark *m_backRight;
+
     public:
-    frc::Spark m_frontLeft;
-    frc::Spark m_backLeft;
-    frc::Spark m_frontRight;
-    frc::Spark m_backRight;
+
+    //create public header
+    drive(){
+        std::cout << "WARNING! No ports defined for drive class constructor." <<  std::endl;
+    }
+
+    drive(int frontLeft, int backLeft, int frontRight, int backRight){
+        m_frontLeft = new Spark(frontLeft);
+        m_frontRight = new Spark(frontRight);
+        m_backLeft = new Spark(backLeft);
+        m_backRight = new Spark(backRight);
+    }
 };
 
-
-void Drive(int frontLeft, int backLeft, int frontRight, int backRight){
+//deprecated... for now
+/* void Drive(int frontLeft, int backLeft, int frontRight, int backRight){
     Spark* m_frontLeft = new Spark(frontLeft);
     Spark* m_backleft = new Spark(backLeft);
     Spark* m_frontRight = new Spark(frontRight);
@@ -21,4 +37,4 @@ void Drive(int frontLeft, int backLeft, int frontRight, int backRight){
 
 void update(Gamepad _gamepad){
     
-};
+}; */
